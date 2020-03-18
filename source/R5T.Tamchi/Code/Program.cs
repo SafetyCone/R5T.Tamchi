5ddcd2fdb8c734d2e138924d9879058b8e086ffe
MinexAutomation <minex.automation@gmail.com>
2020-03-17 20:22:24 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Tamchi/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Tamchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
